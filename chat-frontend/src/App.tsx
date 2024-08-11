100ed836ab71e6f24e497b72b28ebe19be843fe7
ilzat <ilzat.sadykov.2006@bk.ru>
2024-08-11 23:50:53 +0600

    'Взял за основу свой старый проект и переделал на mui + мой api '

--- chat-frontend/src/App.tsx
+++ chat-frontend/src/App.tsx
@@ -1,8 +1,54 @@
 import './App.css'
+import {AxiosError} from 'axios';
+import {useEffect, useState} from 'react';
+import {Message, newMessage} from './types.ts';
+import Messages from './Components/Messages/Messages.tsx';
+import SendMessage from './Components/Messages/SendMessage.tsx';
+import axiosApi from './axiosApi.ts';
 
-const App = () => (
-    <>
-    </>
-);
+
+const App = () => {
+    const [posts, setPosts] = useState<Message[]>([]);
+    const [error, setError] = useState<Error | null>(null);
+
+    const fetchData = async () => {
+        try {
+            const result = await axiosApi.get("/messages");
+            setPosts(result.data);
+        } catch (error) {
+          console.error(error);
+        }
+    };
+    useEffect(() => {
+        void fetchData();
+        const threeSecond = setInterval(fetchData, 3000);
+        return () => clearInterval(threeSecond);
+    }, []);
+
+    const sendNewMessage = async (newMessage: newMessage) => {
+        try {
+           await axiosApi.post('/messages' ,newMessage)
+            void fetchData()
+        } catch (error) {
+            setError(error as AxiosError);
+        }
+    }
+
+    if (error) {
+        return <div>{error.message}</div>;
+    } else
+        return (
+            <>
+                <div className="flex-fill">
+                    {posts.map(post => (
+                        <Messages post={post} key={post.id}/>
+                    ))}
+                </div>
+                <div className='ms-5'>
+                    <SendMessage onSendMessage={sendNewMessage}/>
+                </div>
+            </>
+        )
+}
 
 export default App
---
+++ chat-frontend/src/Components/Messages/SendMessage.tsx
@@ -0,0 +1,64 @@
+import React, {FormEvent, useState} from 'react';
+import {newMessage} from '../../types.ts';
+import {Button, Grid, TextField} from '@mui/material';
+
+interface Props {
+  onSendMessage: (message: newMessage) => void;
+}
+
+const SendMessage: React.FC<Props> = ({onSendMessage}) => {
+  const [state, setState] = useState<newMessage>({
+    author: '',
+    message: '',
+  })
+  const submitMessage = (e: FormEvent) => {
+    e.preventDefault();
+    onSendMessage({...state});
+    setState({author: '', message: ''});
+  }
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = event.target;
+    setState(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <Grid container direction="column" spacing={2} component="form" onSubmit={submitMessage}>
+      <h3>Send Message</h3>
+      <Grid item>
+        <TextField
+          required
+          label="Author"
+          id="author"
+          name="author"
+          value={state.author}
+          onChange={inputChangeHandler}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          required
+          multiline
+          minRows={3}
+          label="Message"
+          id="message"
+          name="message"
+          value={state.message}
+          onChange={inputChangeHandler}
+        />
+      </Grid>
+      <Grid item>
+        <Button
+          type="submit"
+          variant="contained"
+        >
+          <span>Send</span>
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SendMessage;
